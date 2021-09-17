b1cf93f9a06a6b9398200b37bf79a0adf2034cd2
Max Dussen <414805@student.fontys.nl>
2021-09-17 10:44:03 +0200

    Added tests

---
+++ S3P2_project/PostService.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PostService.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int test = 1;
+
+            Assert.IsTrue(test == 1);
+        }
+    }
+}
