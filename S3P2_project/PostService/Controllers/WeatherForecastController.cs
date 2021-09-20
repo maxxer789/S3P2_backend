b2d19cd01c1f7c9fa619a198b3dfe6f92c3c7ebf
maxxer789 <maxvdd76@gmail.com>
2021-09-20 09:39:28 +0200

    Update WeatherForecastController.cs

--- S3P2_project/PostService/Controllers/WeatherForecastController.cs
+++ S3P2_project/PostService/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace PostService.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Brr", "Yikes", "Lit", "Wack", "Chill"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
