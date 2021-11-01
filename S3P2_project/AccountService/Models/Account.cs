d8f6e34593d47c30e38a9676a03761061efeae2a
maxxer789 <maxvdd76@gmail.com>
2021-11-01 13:45:10 +0100

    Account props

--- S3P2_project/AccountService/Models/Account.cs
+++ S3P2_project/AccountService/Models/Account.cs
@@ -7,5 +7,8 @@ namespace AccountService.Models
 {
     public class Account
     {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
     }
 }
