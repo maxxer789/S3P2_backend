f915d5af05b35145bda542a297ed91a0a315bab1
maxxer789 <maxvdd76@gmail.com>
2021-11-02 12:02:09 +0100

    temp remove

--- S3P2_project/PostService/Repositories/PostRepo.cs
+++ S3P2_project/PostService/Repositories/PostRepo.cs
@@ -7,20 +7,22 @@ namespace PostService.Repositories
 {
     public class PostRepo : IPostRepo
     {
-        private readonly PostContext _context;
-        public PostRepo(PostContext context)
+        //private readonly PostContext _context;
+        public PostRepo()
         {
-            _context = context;
+            //_context = context;
         }
 
         public Post GetPostById(int id)
         {
-            return _context.Post.Where(p => p.Id == id).FirstOrDefault();
+            return new Post();
+            //return _context.Post.Where(p => p.Id == id).FirstOrDefault();
         }
 
         public IEnumerable<Post> GetPosts()
         {
-            return _context.Post.AsEnumerable();
+            return new List<Post>();
+            //return _context.Post.AsEnumerable();
         }
     }
 }
