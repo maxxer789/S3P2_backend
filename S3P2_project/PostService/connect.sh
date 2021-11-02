fd6490b36043c555ece03c58ffaa54b45385fa61
maxxer789 <maxvdd76@gmail.com>
2021-11-02 13:49:09 +0100

    test new connect method

--- S3P2_project/PostService/connect.sh
+++ S3P2_project/PostService/connect.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-echo $ANYCONNECT_PASSWORD|openconnect $ANYCONNECT_SERVER --user=$ANYCONNECT_USER --no-cert-check -b
 
+if [ -z "$VPN_SERVER_CERT" ]
+then
+    echo $VPN_PASSWORD |openconnect -u $VPN_LOGIN $VPN_SERVER --passwd-on-stdin -b
+else
+    echo $VPN_PASSWORD |openconnect -u $VPN_LOGIN $VPN_SERVER --servercert $VPN_SERVER_CERT --passwd-on-stdin -b
+fi
+
+# Wait a little
 sleep 5
 
-iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
-iptables -A FORWARD -i eth0 -j ACCEPT
-
-route del -net 172.17.0.0 netmask 255.255.240.0  dev tun0
-
-/bin/bash
+# Print IP
+curl http://api.ipify.org
